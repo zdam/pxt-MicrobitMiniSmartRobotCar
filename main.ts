22bbbcdda6b293109761b49fd283b919d395eec4
Adam Webber <adam.k.webber@gmail.com>
2020-07-29 10:36:04 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,13 @@ function tester () {
         `)
 }
 tester()
+let strip = neopixel.create(DigitalPin.P5, 18, NeoPixelMode.RGB)
+strip.clear()
 basic.forever(function () {
-	
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(200)
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.pause(200)
+    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    basic.pause(200)
 })
